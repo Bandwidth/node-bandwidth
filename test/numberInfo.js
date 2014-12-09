e68fe5fbde9c6417bbe437012508814bb2a263a2
Andrey Belchikov <belchikovs@gmail.com>
2014-12-09 00:10:26 -0800

    Added NumberInfo

---
+++ test/numberInfo.js
@@ -0,0 +1,41 @@
+var lib = require("../");
+var helper = require("./helper");
+var nock = require("nock");
+var NumberInfo = lib.NumberInfo;
+
+describe("NumberInfo", function(){
+  before(function(){
+    nock.disableNetConnect();
+    helper.setupGlobalOptions();
+  });
+  after(function(){
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+  describe("#get", function(){
+    var item = {
+      number: "11111"
+    };
+    it("should return number info", function(done){
+      helper.nock().get("/v1/phoneNumbers/numberInfo/11111").reply(200, item);
+      NumberInfo.get(helper.createClient(), "11111", function(err, i){
+        if(err){
+          return done(err);
+        }
+        i.should.eql(item);
+        done();
+      });
+    });
+    it("should return numberInfo info (with default client)", function(done){
+      helper.nock().get("/v1/phoneNumbers/numberInfo/11111").reply(200, item);
+      debugger;
+      NumberInfo.get("11111", function(err, i){
+        if(err){
+          return done(err);
+        }
+        i.should.eql(item);
+        done();
+      });
+    });
+  });
+});
