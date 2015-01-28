029126e694084cfa9ebf892e47d8419145142128
Andrey Belchikov <belchikovs@gmail.com>
2015-01-28 06:55:02 -0800

    Added domains and endpoints support (not debugged yet)

---
+++ test/endPoint.js
@@ -0,0 +1,26 @@
+"use strict";
+var lib = require("../");
+var helper = require("./helper");
+var nock = require("nock");
+var EndPoint = lib.EndPoint;
+
+describe("EndPoint", function(){
+  before(function(){
+    nock.disableNetConnect();
+    helper.setupGlobalOptions();
+  });
+  after(function(){
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+  describe("#delete", function(){
+    it("should remove an endpoint", function(done){
+      helper.nock().del("/v1/users/FakeUserId/domains/1/endpoints/10").reply(200);
+      var endPoint = new EndPoint();
+      endPoint.domainId = 1;
+      endPoint.id = 10;
+      endPoint.client = helper.createClient();
+      endPoint.delete(done);
+    });
+  });
+});
