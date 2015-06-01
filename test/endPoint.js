25f08979f0d9b1e531fff58ae69a6606e2924c2d
Nathan Fuchs <nfuchs@bandwidth.com>
2015-06-01 10:24:09 -0400

    add tests for createToken

--- test/endPoint.js
+++ test/endPoint.js
@@ -23,4 +23,34 @@ describe("EndPoint", function(){
       endPoint.delete(done);
     });
   });
+  describe("#createAuthToken - success", function(){
+    it("should create an auth token", function(done){
+      var tokenData = {
+        token: '1234567890',
+        expires: 86400
+      };
+      helper.nock().post("/v1/users/FakeUserId/domains/1/endpoints/10/tokens").reply(200, tokenData);
+      var endPoint = new EndPoint();
+      endPoint.domainId = 1;
+      endPoint.id = 10;
+      endPoint.client = helper.createClient();
+      endPoint.createAuthToken(function(err, data){
+        data.should.eql(tokenData);
+        done(err);
+      });
+    });
+  });
+  describe("#createAuthToken - failure", function(){
+    it("should fail", function(done){
+      helper.nock().post("/v1/users/FakeUserId/domains/1/endpoints/10/tokens").reply(400);
+      var endPoint = new EndPoint();
+      endPoint.domainId = 1;
+      endPoint.id = 10;
+      endPoint.client = helper.createClient();
+      endPoint.createAuthToken(function(err, data){
+        err.should.not.eql(null);
+        done();
+      });
+    });
+  });
 });
