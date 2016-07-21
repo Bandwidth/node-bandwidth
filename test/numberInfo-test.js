d00f537daf57aaecea78b04cb24da71840b56d32
Andrey Belchikov <belchikovs@gmail.com>
2016-07-21 11:48:41 +0300

    NumberInfo API

---
+++ test/numberInfo-test.js
@@ -0,0 +1,50 @@
+var nock = require("nock");
+var CatapultClient = require("../index");
+
+var baseUrl = "https://api.catapult.inetwork.com";
+
+describe("NumberInfo API", function () {
+
+	describe("global methods", function () {
+		var client;
+
+		var userId = "fakeUserId";
+		var apiToken = "fakeApiToken";
+		var apiSecret = "fakeapiSecret";
+
+		var number = "234567890";
+
+		var testNumberInfo = {
+			"created" : "2013-09-23T16:31:15Z",
+			"name"    : "Name",
+			"number"  : "{number}",
+			"updated" : "2013-09-23T16:42:18Z"
+		};
+
+		before(function () {
+			client = new CatapultClient({
+				userId    : userId,
+				apiToken  : apiToken,
+				apiSecret : apiSecret
+			});
+			nock.disableNetConnect();
+
+			nock("https://api.catapult.inetwork.com")
+				.persist()
+				.get("/v1/numberInfo/" + number)
+				.reply(200, testNumberInfo);
+		});
+
+		after(function () {
+			nock.cleanAll();
+			nock.enableNetConnect();
+		});
+
+		it("should get a number info, promise style", function () {
+			return client.NumberInfo.get(number)
+			.then(function (numberInfo) {
+				numberInfo.should.eql(testNumberInfo);
+			});
+		});
+	});
+});
