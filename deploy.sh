eaa935e18e0d8faf6d8f66171de6afae46fa2b64
Illirik Smirnov <hi@illiriksmirnov.com>
2016-08-04 10:37:16 -0400

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ function docGen {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ "TRAVIS_NODE_VERSION" != "5" ]; then
     echo "Skipping deploy; just doing a build."
     exit 0
 fi
