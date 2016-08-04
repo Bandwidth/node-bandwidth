5b1b6154904d122258ecfc2f32295e62915326e5
Illirik Smirnov <hi@illiriksmirnov.com>
2016-08-04 10:11:48 -0400

    Let's run the script now.

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,10 @@ function docGen {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    exit 0
-fi
+#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#    echo "Skipping deploy; just doing a build."
+#    exit 0
+#fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
