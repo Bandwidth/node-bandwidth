b73175800622e4b0b87eca9787481626f67fb800
Andrey Belchikov <belchikovs@gmail.com>
2018-02-01 11:33:12 +0300

    Added tests for play audio

---
+++ test/extensions/play-audio.test.js
@@ -0,0 +1,77 @@
+import test from 'ava';
+import td from 'testdouble';
+import playAudioExtensions from '../../dist/extensions/play-audio';
+
+test('speakSententence() should call playAudio() with right params', async t => {
+	const Call = {playAudio: td.function()};
+	td.when(Call.playAudio({id: 'id', sentence: 'hello'}, null)).thenResolve();
+	playAudioExtensions(Call);
+	await Call.speakSententence.execute.call(Call, 'id', 'hello');
+	t.pass();
+});
+
+test('playFileUrl() should call playAudio() with right params', async t => {
+	const Call = {playAudio: td.function()};
+	td.when(Call.playAudio({id: 'id', fileUrl: 'url'}, null)).thenResolve();
+	playAudioExtensions(Call);
+	await Call.playFileUrl.execute.call(Call, 'id', 'url');
+	t.pass();
+});
+
+test('stopPlayFileUrl() should call playAudio() with right params', async t => {
+	const Call = {playAudio: td.function()};
+	td.when(Call.playAudio({id: 'id', fileUrl: ''}, null)).thenResolve();
+	playAudioExtensions(Call);
+	await Call.stopPlayFileUrl.execute.call(Call, 'id');
+	t.pass();
+});
+
+test('speakSententenceToMember() should call playAudio() with right params', async t => {
+	const Call = {playAudioToMember: td.function(), playAudio: td.function()};
+	td
+		.when(
+			Call.playAudioToMember(
+				{id: 'id', memberId: 'memberId', sentence: 'hello'},
+				null
+			)
+		)
+		.thenResolve();
+	playAudioExtensions(Call);
+	await Call.speakSententenceToMember.execute.call(
+		Call,
+		'id',
+		'memberId',
+		'hello'
+	);
+	t.pass();
+});
+
+test('playFileUrlToMember() should call playAudio() with right params', async t => {
+	const Call = {playAudioToMember: td.function(), playAudio: td.function()};
+	td
+		.when(
+			Call.playAudioToMember(
+				{id: 'id', memberId: 'memberId', fileUrl: 'url'},
+				null
+			)
+		)
+		.thenResolve();
+	playAudioExtensions(Call);
+	await Call.playFileUrlToMember.execute.call(Call, 'id', 'memberId', 'url');
+	t.pass();
+});
+
+test('stopPlayFileUrlToMember() should call playAudio() with right params', async t => {
+	const Call = {playAudioToMember: td.function(), playAudio: td.function()};
+	td
+		.when(
+			Call.playAudioToMember(
+				{id: 'id', memberId: 'memberId', fileUrl: ''},
+				null
+			)
+		)
+		.thenResolve();
+	playAudioExtensions(Call);
+	await Call.stopPlayFileUrlToMember.execute.call(Call, 'id', 'memberId');
+	t.pass();
+});
