7f96bbf0f38385aefcb2eb002524fd0f100e18d1
Andrey Belchikov <belchikovs@gmail.com>
2018-02-07 16:47:25 +0300

    Added comments to types to make autocompletion more informative

--- tools/prepare-types.js
+++ tools/prepare-types.js
@@ -27,7 +27,7 @@ function printProperties(prefix, properties) {
 	return Object.keys(properties)
 		.map(k => {
 			const schema = properties[k];
-			let text = ``;
+			let text = `\t/** ${schema.description || k} */\n`;
 			switch (schema.type) {
 				case 'object':
 					text += `\t${k}: ${prefix}${_.upperFirst(k)}`;
@@ -79,9 +79,9 @@ ${printProperties(type, schema.properties)}
 		.join('\n');
 }
 
-function printApiName(name) {
+function printApiName(name, data) {
 	name = _.upperFirst(name);
-	return `\t${name}: ${name}`;
+	return `\t/** ${data._description || `${name} API`} */\n\t${name}: ${name}`;
 }
 
 function extractParamsFromPath(path) {
@@ -184,7 +184,9 @@ function printApiMethod(apiName, name, data) {
 			anyOf ? ` | ${typeName}[]` : ''
 		}, `;
 	}
-	return `\t${name}(${paramsDesclaration}cancelToken?: CancelToken): Promise<${getOutputTypes(
+	return `\t/** ${
+		data._description
+	} */\n\t${name}(${paramsDesclaration}cancelToken?: CancelToken): Promise<${getOutputTypes(
 		`${_.upperFirst(apiName)}${_.upperFirst(name)}`,
 		data,
 		params
@@ -193,7 +195,7 @@ function printApiMethod(apiName, name, data) {
 
 function printApiTypes(name, data) {
 	name = _.upperFirst(name);
-	return `export interface ${name} {
+	return `/** ${data._description} */\nexport interface ${name} {
 ${Object.keys(data)
 		.filter(m => !m.startsWith('_'))
 		.map(m => printApiMethod(name, m, data[m]))
@@ -219,69 +221,102 @@ ${Object.keys(apiData)
 ${printTypes()}
 
 export interface Bridges {
+	/** Speak a sentence to the bridge */
 	speakSententence(id: string, sentence: string, options?: SpeakSententenceOptions, cancelToken?: CancelToken): Promise<any>;
+	/** Play audio file to the bridge */
 	playFileUrl(id: string, fileUrl: string, options?: PlayFileUrlOptions, cancelToken?: CancelToken): Promise<any>;
+	/** Stop playing of audio file to the bridge */
 	stopPlayFileUrl(id: string, cancelToken?: CancelToken): Promise<any>;
 }
 
 export interface Calls {
+	/** Answer incoming call */
 	answer(id: string, cancelToken?: CancelToken): Promise<any>;
+	/** Cancel incoming call */
 	terminate(id: string, cancelToken?: CancelToken): Promise<any>;
+	/** Complete the active call */
 	hangup(id: string, cancelToken?: CancelToken): Promise<any>;
+	/** Transfer the call */
 	transfer(id: string, transferTo: string, options?: TransferOptions, cancelToken?: CancelToken): Promise<string>;
+	/** Stop collection of gather data */
 	stopGather(id: string, gatherId: string, cancelToken?: CancelToken): Promise<any>;
+	/** Speak a sentence to the call */
 	speakSententence(id: string, sentence: string, options?: SpeakSententenceOptions, cancelToken?: CancelToken): Promise<any>;
+	/** Play audio file to the call */
 	playFileUrl(id: string, fileUrl: string, options?: PlayFileUrlOptions, cancelToken?: CancelToken): Promise<any>;
+	/** Stop playing of audio file to the call */
 	stopPlayFileUrl(id: string, cancelToken?: CancelToken): Promise<any>;
 }
 
 export interface Conferences {
+	/** Stop the conference */
 	stop(id: string, cancelToken?: CancelToken): Promise<any>;
+	/** Mute or unmute the conference */
 	mute(id: string, mute?: boolean, cancelToken?: CancelToken): Promise<any>;
+	/** Hold or unhold the conference */
 	hold(id: string, hold?: boolean, cancelToken?: CancelToken): Promise<any>;
+	/** Remove a member from the conference */
 	deleteMember(id: string, memberId: string, cancelToken?: CancelToken): Promise<any>;
+	/** Mute or unmute the conference member */
 	muteMember(id: string, memberId: string, mute?: boolean, cancelToken?: CancelToken): Promise<any>;
+	/** Hold or unhold the conference member */
 	holdMember(id: string, memberId: string, hold?: boolean, cancelToken?: CancelToken): Promise<any>;
+	/** Speak a sentence to the conference */
 	speakSententence(id: string, sentence: string, options?: SpeakSententenceOptions, cancelToken?: CancelToken): Promise<any>;
+	/** Play audio file to the conference */
 	playFileUrl(id: string, fileUrl: string, options?: PlayFileUrlOptions, cancelToken?: CancelToken): Promise<any>;
+	/** Stop playing of audio file to the conference */
 	stopPlayFileUrl(id: string, cancelToken?: CancelToken): Promise<any>;
+	/** Speak a sentence to the conference member*/
 	speakSententenceToMember(id: string, memberId: string, sentence: string, options?: SpeakSententenceOptions, cancelToken?: CancelToken): Promise<any>;
+	/** Play audio file to the conference member*/
 	playFileUrlToMember(id: string, memberId: string, fileUrl: string, options?: PlayFileUrlOptions, cancelToken?: CancelToken): Promise<any>;
+	/** Stop playing of audio file to the conference member*/
 	stopPlayFileUrlToMember(id: string, memberId: string, cancelToken?: CancelToken): Promise<any>;
 }
 
 export interface PlayAudioOptions {
+	/** URL to audio file to play */
 	fileUrl: string;
+	/** A sentence to speak */
 	sentence: string;
+	/** A gender of voice to speak a sentence */
 	gender: 'female' | 'male';
+	/** A locale of voice to speak a sentence */
 	locale: string;
+	/** A voice name to speak a sentence */
 	voice: string;
+	/** Additional data for callback */
 	tag: string;
 	id?: string;
 }
 
 export interface SpeakSententenceOptions {
+	/** A gender of voice to speak a sentence */
 	gender: 'female' | 'male';
+	/** A locale of voice to speak a sentence */
 	locale: string;
+	/** A voice name to speak a sentence */
 	voice: string;
+	/** Additional data for callback */
 	tag: string;
 }
 
 export interface PlayFileUrlOptions {
+	/** Additional data for callback */
 	tag: string;
 }
 
 export interface TransferOptions {
-	callTimeout: number;
-	callbackTimeout: number;
+	/** The server URL where the call events for the new call will be sent upon transferring. */
 	callbackUrl: string;
-	callbackHttpMethod: 'GET' | 'POST';
-	fallbackUrl: string;
+	/** Indicates if the call should be recorded. Values true or false. You can turn recording on/off and have multiple recordings on a single call. */
 	recordingEnabled: boolean;
+	/** The file format of the recorded call. Supported values are wav (default) and mp3. */
 	recordingFileFormat: 'wav' | 'mp3';
-	recordingMaxDuration: number;
-	transcriptionEnabled: boolean;
+	/** This is the caller id that will be used when the call is transferred. */
 	transferCallerId: string;
+	/** Audio to be played to the caller that the call will be transferred to.  */
 	whisperAudio: PlayAudioOptions;
 }
 
@@ -292,8 +327,11 @@ ${Object.keys(apiData)
 }
 
 export interface BandwidthApiOptions {
+	/** Bandwidth API user ID (not user name) */
 	userId: string;
+	/** Bandwidth API token */
 	apiToken: string;
+	/** Bandwidth API secret */
 	apiSecret: string;
 }
 
